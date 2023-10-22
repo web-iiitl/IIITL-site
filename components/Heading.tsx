94b6cc7e09bd638f4d7f39115e6320a89c1e4ecd
sarthak ganure <sarthakganure@sarthaks-MacBook-Air.local>
2023-10-22 16:36:46 +0530

    title animation updated

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,10 +1,12 @@
 import Head from "./Heading_transition";
+import 'animate.css';
 
 
 const Heading = () => {
   return (
-    <div>
+    <div className="animate__bounce">
         <Head className=" text-center justify-center items-center"/>
+        <span className="text-xl float-right animate__zoomOutRight">(An Institute of National Importance by the Act of Parliament)</span>
     </div>
   )
 }
