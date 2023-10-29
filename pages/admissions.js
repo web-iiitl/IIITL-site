71a46dce67ae573cde1003e55ff499273b828af6
atulc036 <lcb2022036@iiitl.ac.in>
2023-10-29 18:27:12 +0530

    Added underline effects to courses.

--- pages/admissions.js
+++ pages/admissions.js
@@ -4,6 +4,7 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 import Link from 'next/link';
 import PortableText from "react-portable-text"
+import styles from './admissions.module.css'
 
 const admissions = ({ admissions,course }) => {
     const client = createClient({
@@ -24,26 +25,26 @@ const admissions = ({ admissions,course }) => {
                 {
                     admissions.map((item, index) => {
                         return (
-                            <div className='p-2 shadow-lg'>
+                            <div className='p-4 shadow-lg mb-4'>
                                 <h1 className='text-5xl text-cyan-800 font-semibold mb-4 border-b-2 border-cyan-800'>{item.title}</h1>
                                 <PortableText
                                     content={item.content}
                                     serializers={{
                                     }}
                                 />
-                                <h2 className='my-5 text-xl font-semibold text-cyan-700 border-b-2'>Courses</h2>
+                                <h2 className='my-5 text-2xl font-semibold text-cyan-700 border-b-2 border-cyan-800 text-center'>Courses</h2>
 
-                                <div>
+                                <div className='flex gap-5 justify-center mb-4'>
                                  {
                                     item.course?.map((it,ind)=>{
                                         return(
-                                            <div>
+                                            <div className='text-2xl'>
                                              {
                                                  course.map((cd,i)=>{
                                                     if(cd._id==it._ref){
                                                         return(
-                                                            <div>
-                                                             <Link className='font-semibold bold text-cyan-700 ml-2' href={'/course/'+cd._id}>
+                                                            <div className={styles.course}>
+                                                             <Link className='font-semibold bold text-cyan-700 ml-2 no-underline' href={'/course/'+cd._id}>
                                                                 {cd.title}
                                                              </Link>
                                                             </div>
