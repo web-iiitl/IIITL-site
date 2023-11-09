9532578553b90c8cbd5bdf22b15b50803f922b32
atulc036 <lcb2022036@iiitl.ac.in>
2023-11-09 18:48:13 +0530

    The New gif is changed as per the old website

--- components/Marque.tsx
+++ components/Marque.tsx
@@ -9,10 +9,10 @@ const marque = () => {
           pauseOnHover={true}
         >
           <span className="inline-block h-6 w-9">
-            <img src="https://www.lordbuddhagroup.com/wp-content/uploads/2018/08/NEW-GIF.gif" alt="" />
+            <img src="./new.gif" alt="" />
           </span> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam, eaque.
           <span className="inline-block h-6 w-9">
-            <img src="https://www.lordbuddhagroup.com/wp-content/uploads/2018/08/NEW-GIF.gif" alt="" />
+            <img src="./new.gif" alt="" />
           </span>
         </Marquee>
     </div>
