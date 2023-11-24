48e2ccc6f4f7bf5c760a3b2469bd9f987f543baa
sarthak ganure <sarthakganure@sarthaks-MacBook-Air.local>
2023-11-24 21:51:35 +0530

    people edit

--- pages/people/[id].js
+++ pages/people/[id].js
@@ -25,52 +25,51 @@ const people = ({ people }) => {
       {people.map((it, index) => {
         if (it._id == id) {
           return (
-            <div key={index} className="flex w-full min-h-[700px]">
-              <div className="w-1/4 px-5 py-10 text-center items-center flex flex-col bg-[#375F9B]">
-                <img
-                  src={urlFor(it.picture)}
-                  className="max-w-[250px] rounded-full"
-                  alt=""
-                />
-                <h2 className="my-3 mb-10 text-2xl text-white font-semibold">
-                  {`${it.name}`}
-                  
-                </h2>
-                <span className="text-white text-xl font-medium">{`(${it.post})`}</span>
-                {it.qualifications.map((item, ind) => {
-                  return (
-                    <button
-                      key={ind}
-                      className="py-4 bg-[#426ca9] text-white hover:font-black w-full font-medium text-2xl  my-5 shadow-sm hover:scale-105 rounded-md duration-300"
-                    >
-                      <Link
-                        to={item.title}
-                        activeClass="active"
-                        spy={true}
-                        smooth={true}
-                        offset={-60}
-                        duration={70}
-                        className="text-inherit no-underline"
+            <div key={index} className="flex w-full min-h-[400px]">
+              <div className="w-1/4 px-5 py-4 text-center flex flex-col bg-[#375F9B] scroll-m-10">
+                <div className="sticky top-0">
+                  <img
+                    src={urlFor(it.picture)}
+                    className="max-w-[200px] rounded-full"
+                    alt=""
+                  />
+                  <h2 className="my-3 mb-10 text-2xl text-white font-semibold">
+                    {`${it.name}`}
+                  </h2>
+                  <span className="text-white text-xl font-medium">{`(${it.post})`}</span>
+                  <div className="h-[300px] overflow-y-auto my-3">
+                    {it.qualifications.map((item, ind) => (
+                      <button
+                        key={ind}
+                        className="py-3 bg-[#426ca9] text-white hover:font-black w-full font-medium text-xl  my-2 shadow-sm hover:scale-105 rounded-md duration-300"
                       >
-                        {item.title}
-                      </Link>
-                    </button>
-                  );
-                })}
+                        <Link
+                          to={item.title}
+                          activeClass="active"
+                          spy={true}
+                          smooth={true}
+                          offset={-60}
+                          duration={70}
+                          className="text-inherit no-underline"
+                        >
+                          {item.title}
+                        </Link>
+                      </button>
+                    ))}
+                  </div>
+                </div>
               </div>
-              <div className="p-2">
-                {it.qualifications.map((item, ind) => {
-                  return (
-                    <div key={ind} id={item.title}>
-                      <h2 className="font-serif lg:text-3xl text-bluel md:text-5xl px-10 pt-5 sm:text-4xl max-sm:text-2xl border-b-2 border-gray-400 font-semibold">
-                        {item.title}
-                      </h2>
-                      <div className="px-10 py-4 text-xl">
-                        <PortableText content={item.text} serializers={{}} />
-                      </div>
+              <div className="p-2 flex-grow">
+                {it.qualifications.map((item, ind) => (
+                  <div key={ind} id={item.title}>
+                    <h2 className="font-serif lg:text-3xl text-bluel md:text-5xl px-10 pt-5 sm:text-4xl max-sm:text-2xl border-b-2 border-gray-400 font-semibold">
+                      {item.title}
+                    </h2>
+                    <div className="px-10 py-4 text-xl">
+                      <PortableText content={item.text} serializers={{}} />
                     </div>
-                  );
-                })}
+                  </div>
+                ))}
               </div>
             </div>
           );
